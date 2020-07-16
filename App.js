13788cbe0a81c18bf7d96e79920802b3ae7105b3
Zachary Langley <zaclangley04@gmail.com>
2020-07-16 12:04:11 -0600

    friend list and private messagings

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import { YellowBox } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
 
-console.ignoredYellowBox = ['Remote Debugger'];
-YellowBox.ignoreWarnings([])
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createSocketIoMiddleware from "redux-socket.io";
+import io from "socket.io-client";
+import { YellowBox } from "react-native";
+import AppContainer from "./AppContainer";
 
-export default App = () => {
+console.ignoredYellowBox = ["Remote debugger"];
+YellowBox.ignoreWarnings([
+  "Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?"
+]);
+
+const socket = io("http://192.168.1.104:3001");
+const socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
+
+function reducer(state = { conversations: {} }, action) {
+  switch (action.type) {
+    case "users_online":
+      const conversations = { ...state.conversations };
+      const usersOnline = action.data;
+      for (let i = 0; i < usersOnline.length; i++) {
+        const userId = usersOnline[i].userId;
+        if (conversations[userId] === undefined) {
+          conversations[userId] = {
+            messages: [],
+            username: usersOnline[i].username
+          };
+        }
+      }
+      return { ...state, usersOnline, conversations };
+    case "private_message":
+      const conversationId = action.data.conversationId;
+      return {
+        ...state,
+        conversations: {
+          ...state.conversations,
+          [conversationId]: {
+            ...state.conversations[conversationId],
+            messages: [
+              action.data.message,
+              ...state.conversations[conversationId].messages
+            ]
+          }
+        }
+      };
+    case "self_user":
+      return { ...state, selfUser: action.data };
+    default:
+      return state;
+  }
+}
+
+const store = applyMiddleware(socketIoMiddleware)(createStore)(reducer);
+
+store.subscribe(() => {
+  console.log("new state", store.getState());
+});
+
+export default function App() {
   return (
-    <HomeScreen />
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
   );
 }
