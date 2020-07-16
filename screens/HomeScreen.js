d7df5f51281f118d8082892ff9398484bac0015e
Zachary Langley <zaclangley04@gmail.com>
2020-07-15 20:16:08 -0600

    gifted chat implementation and proper message interpretation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, useRef } from "react";
+import { View, Platform, KeyboardAvoidingView } from "react-native";
+import io from "socket.io-client";
+import { GiftedChat } from "react-native-gifted-chat";
+
+export default function HomeScreen() {
+  const [recvMessages, setRecvMessages] = useState([]);
+  const socket = useRef(null);
+
+  useEffect(() => {
+    socket.current = io("http://192.168.1.104:3001/");
+    socket.current.on("message", message => {
+      debugger;
+      console.log("message", message);
+      setRecvMessages(prevState => GiftedChat.append(prevState, message));
+    });
+  }, []);
+
+  const onSend = messages => {
+    console.log(messages);
+    socket.current.emit("message", messages[0].text);
+    setRecvMessages(prevState => GiftedChat.append(prevState, messages));
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <GiftedChat
+        messages={recvMessages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+    </View>
+  );
+}
